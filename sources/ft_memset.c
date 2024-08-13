714c95d211f330bba4c6635560e34699012c6d89
AndranikAdyan <andranikadyan@gmail.com>
2024-08-13 18:59:57 +0400

    ft_memset last fix (I hope)

--- sources/ft_memset.c
+++ sources/ft_memset.c
@@ -6,12 +6,11 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 20:57:42 by aadyan            #+#    #+#             */
-/*   Updated: 2024/08/13 18:43:42 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/08/13 18:59:16 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "string.h"
 
 void	*memset(void *ptr, int value, size_t num)
 {
