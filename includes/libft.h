c17e7ed1b736f2bd23bb1474c7db5e8f8f894673
AndranikAdyan <andranikadyan@gmail.com>
2024-08-24 16:44:33 +0400

    libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:55:12 by aadyan            #+#    #+#             */
-/*   Updated: 2024/08/01 20:07:58 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/08/24 16:43:50 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 
 # include <stdio.h>
 
-int	ft_isalpha(int ch);
+void	ft_bzero(void *s, size_t n);;
+int		ft_isalnum(int ch);
+int		ft_isalpha(int ch);
+int		ft_isascii(int ch);
+int		ft_isdigit(int ch);
+int		ft_isprint(int ch);
+int		ft_isprint(int ch);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *ptr, int value, size_t num);
+int		ft_strlen(const char *str);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
