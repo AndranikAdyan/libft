04d47344fdb66abdf5a1d7476257dc94cd00ef53
AndranikAdyan <andranikadyan@gmail.com>
2024-08-28 18:42:35 +0400

    ft_toupper

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:55:12 by aadyan            #+#    #+#             */
-/*   Updated: 2024/08/28 16:27:53 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/08/28 18:42:25 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	*ft_memset(void *ptr, int value, size_t num);
 int		ft_strlen(const char *str);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-// size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int	ft_toupper(int c);
 
 #endif
