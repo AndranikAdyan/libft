4db28d563d5bc3e37d31d7f99a3d36118af5f2e3
AndranikAdyan <andranikadyan@gmail.com>
2024-08-28 18:47:45 +0400

    ft_lower

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:55:12 by aadyan            #+#    #+#             */
-/*   Updated: 2024/08/28 18:42:25 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/08/28 18:47:32 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int		ft_strlen(const char *str);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-int	ft_toupper(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
