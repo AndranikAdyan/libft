a4119aac1c6b72b81abc8c0db89cc9d9955ababe
AndranikAdyan <andranikadyan@gmail.com>
2024-10-06 20:30:39 +0400

    fixing

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:55:12 by aadyan            #+#    #+#             */
-/*   Updated: 2024/08/29 00:22:25 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/06 20:29:06 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdio.h>
+# include <stdlib.h>
 
 void	ft_bzero(void *s, size_t n);;
 int		ft_isalnum(int ch);
@@ -21,15 +22,19 @@ int		ft_isalpha(int ch);
 int		ft_isascii(int ch);
 int		ft_isdigit(int ch);
 int		ft_isprint(int ch);
-int		ft_isprint(int ch);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *ptr, int value, size_t num);
-int		ft_strlen(const char *str);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strnstr(char *str, char *to_find, int n);
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
+char	*ft_strdup(char *src);
+void	*calloc(size_t nmemb, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *str, int ch);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
---
+++ sources/ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 20:09:27 by aadyan            #+#    #+#             */
+/*   Updated: 2024/10/06 20:26:49 by aadyan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void			*arr;
+	unsigned char	*p;
+	size_t			i;
+
+	arr = (void *)malloc(nmemb * size);
+	p = arr;
+	i = 0;
+	while (i < size)
+	{
+		p[i] = 0;
+		++i;
+	}
+	return (arr);
+}
