7f58414a6b7f13e5867672e3ed446647920ad90e
AndranikAdyan <andranikadyan@gmail.com>
2024-10-11 17:27:32 +0400

    add ft_putendl_fd

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:55:12 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/11 17:04:43 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/11 17:19:36 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ char	*ft_itoa(int nb);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
