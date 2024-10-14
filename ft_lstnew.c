b0568157c1872e8b1ae60fd4aab15f480619ea76
AndranikAdyan <andranikadyan@gmail.com>
2024-10-14 17:05:59 +0400

    lstnew update

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.C                                        :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:35:48 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/14 16:12:34 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/14 17:02:54 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*lst;
 
 	lst = (t_list *)malloc(sizeof(t_list));
+	if (!lst)
+		return (NULL);
 	lst->content = content;
 	lst->next = NULL;
 	return (lst);
