5ad155049f568c05ce5da19835c8d1b919adc4f4
AndranikAdyan <andranikadyan@gmail.com>
2024-10-17 23:08:09 +0400

    fix memset name

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 20:57:42 by aadyan            #+#    #+#             */
-/*   Updated: 2024/08/28 15:07:45 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/17 23:07:47 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memset(void *ptr, int value, size_t num)
+void	*ft_memset(void *ptr, int value, size_t num)
 {
 	size_t			i;
 	unsigned char	*p;
