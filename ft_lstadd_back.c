74f71372c480366772ce19f07c9ce26bd89ccce7
AndranikAdyan <andranikadyan@gmail.com>
2024-10-17 21:10:40 +0400

    update lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:29:15 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/14 17:41:04 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/17 21:09:27 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
-	tmp = *lst;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
+	if (lst)
+	{
+		if (!*lst)
+			lst = new;
+		else
+		{
+			tmp = ft_lstlast(*lst));
+			tmp->next = new;
+		}
+	}
 }
