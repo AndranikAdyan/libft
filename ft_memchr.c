ea73b074329d7346353df305b926dd2ce6310d47
AndranikAdyan <andranikadyan@gmail.com>
2024-10-21 18:45:21 +0400

    fix memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 23:39:51 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/06 23:40:09 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/21 18:44:53 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	p = s;
 	while (i < n)
 	{
-		if (p[i] == c)
+		if (p[i] == (unsigned char)c)
 			return ((void *)(p + i));
 		++i;
 	}
