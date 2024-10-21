60f290c5f46bf2aa897778fdfb6949f658fc11a5
AndranikAdyan <andranikadyan@gmail.com>
2024-10-21 19:00:54 +0400

    cleanup strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:48:22 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/21 19:00:11 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/21 19:00:37 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,12 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int			i;
-	// char	*str;
 
-	// i = ft_strlen(s) - 1;
 	i = strlen(s) - 1;
 	while (i >= 0)
 	{
 		if (s[i] == (char)c)
-		{
-			// str = (char *)s + i;
 			return ((char *)s + i);
-		}
 		--i;
 	}
 	if (c == '\0')
