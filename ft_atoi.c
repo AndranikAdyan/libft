bde496f323807997488ba02c2c135b7f3474ed8d
AndranikAdyan <andranikadyan@gmail.com>
2024-10-21 19:38:41 +0400

    fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 00:33:59 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/09 00:53:55 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/21 19:35:36 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@ int	is_digit(char c)
 	return (c >= '0' && c <= '9');
 }
 
+int	is_space(char c)
+{
+	return (c == ' ' || c == '\t' \
+			|| c == '\r' || c == '\v' \
+			|| c == '\f' || c == '\n');
+}
+
 int	ft_atoi(const char *nptr)
 {
 	int	i;
@@ -24,7 +31,7 @@ int	ft_atoi(const char *nptr)
 	int	num;
 
 	i = 0;
-	while (nptr[i] == ' ')
+	while (is_space(nptr[i]))
 		++i;
 	sign = 1;
 	if (!is_digit(nptr[i]))
