3a6528259cb3fc7b4c829bc2bc2ccf48df5ae91b
AndranikAdyan <andranikadyan@gmail.com>
2024-10-21 20:01:19 +0400

    fix calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 20:09:27 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/06 23:39:38 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/21 20:00:23 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t			i;
 
 	arr = (void *)malloc(nmemb * size);
+	if (!arr)
+		return (NULL);
 	p = arr;
 	i = 0;
-	while (i < size)
+	while (i < size * nmemb)
 	{
 		p[i] = 0;
 		++i;
