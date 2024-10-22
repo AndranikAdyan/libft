f838cb8313a3a9b304c621dc603ac38baea864b7
AndranikAdyan <andranikadyan@gmail.com>
2024-10-22 17:33:45 +0400

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,22 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 21:01:17 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/09 21:39:05 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/22 17:30:19 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
 
-	if (!s[0] || start > (unsigned int)ft_strlen(s))
+	if (!s)
 		return (NULL);
+	if (start >= (unsigned int)ft_strlen(s))
+		return (ft_strdup(""));
 	str = (char *)malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
