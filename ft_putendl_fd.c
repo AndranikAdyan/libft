42c33938c252a7de98e5e8f2cb3d1ade6da73208
AndranikAdyan <andranikadyan@gmail.com>
2024-10-23 16:22:10 +0400

    fix putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:18:46 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/11 17:42:54 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/23 16:21:43 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
