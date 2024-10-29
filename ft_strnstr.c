3a139f50a1a163a4e54cf1f2f6a0ff55f5525f81
AndranikAdyan <andranikadyan@gmail.com>
2024-10-29 11:37:08 +0400

    strnstr fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,21 +6,11 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:56:27 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/29 11:29:11 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/29 11:36:55 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		++i;
-	return (i);
-}
 
 char	*ft_strnstr(char *str, char *to_find, int n)
 {
@@ -28,16 +18,17 @@ char	*ft_strnstr(char *str, char *to_find, int n)
 	int		k;
 	int		size;
 
-	if (!*to_find)
-		return (str);
 	if (n == 0)
 		return (NULL);
+	if (!*to_find)
+		return (str);
 	size = ft_strlen(to_find);
 	i = 0;
 	while (str[i] && i < n)
 	{
 		k = 0;
-		while (str[i + k] && to_find[k] && str[i + k] == to_find[k])
+		while (str[i + k] && to_find[k] && \
+			str[i + k] == to_find[k] && i + k < n)
 		{
 			k++;
 		}
