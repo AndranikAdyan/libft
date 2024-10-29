8d21e599589bb89cf9f2d64e877d0cfe51e82abc
AndranikAdyan <andranikadyan@gmail.com>
2024-10-29 11:28:45 +0400

    strnstr fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,21 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:56:27 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/21 19:29:04 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/28 21:15:44 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+// #include "libft.h"
+#include <stddef.h>
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	return (i);
+}
 
 char	*ft_strnstr(char *str, char *to_find, int n)
 {
@@ -18,25 +28,29 @@ char	*ft_strnstr(char *str, char *to_find, int n)
 	int		k;
 	int		size;
 
-	if (!*to_find || n == 0)
+	if (!*to_find)
 		return (str);
+	if (n == 0)
+		return (NULL);
 	size = ft_strlen(to_find);
 	i = 0;
-	k = 0;
 	while (str[i] && i < n)
 	{
-		if (str[i] == to_find[k])
+		k = 0;
+		while (str[i + k] && to_find[k] && str[i + k] == to_find[k])
 		{
 			k++;
-			if (k == size)
-				return (str + i - size + 1);
 		}
-		else
-		{
-			i = i - k;
-			k = 0;
-		}
-		i++;
+		if (k == size)
+			return (str + i);
+		++i;
 	}
 	return (0);
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("%s", ft_strnstr("Hello woworld 1234", "world", 16));
+	return 0;
 }
