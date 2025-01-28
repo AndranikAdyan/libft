a43337bf14b74af9d29a8001783959bde0fe3909
AndranikAdyan <andranikadyan@gmail.com>
2025-01-28 14:58:35 +0400

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 12:11:32 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/21 19:12:31 by aadyan           ###   ########.fr       */
+/*   Updated: 2025/01/28 14:54:51 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		++i;
 	}
 	return (0);
